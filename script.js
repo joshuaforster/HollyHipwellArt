e640236b810772d900fe7ef5819846a60e357e22
joshuaforster <110224330+joshuaforster@users.noreply.github.com>
2023-11-11 19:54:00 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// Navigation Menu
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", function {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach(function {
+  link.addEventListener("click", () function {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  });
+});
+
+
+
+
+
+
+
+
