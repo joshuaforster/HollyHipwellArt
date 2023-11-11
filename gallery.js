8a62ca27a9e487c95d58b6cbab54f45b7def013e
joshuaforster <110224330+joshuaforster@users.noreply.github.com>
2023-11-11 22:34:19 +0000

    push

---
+++ gallery.js
@@ -0,0 +1,20 @@
+
+const galleryImages = [
+    "Images/IMG_0614.png",
+    "Images/IMG_0615.png",
+    "Images/IMG_0620.png",
+    "Images/IMG_0617.png",
+    "Images/IMG_0618.png",
+    "Images/IMG_0616.png",
+]
+
+
+
+const imageGallery = document.getElementById('image-gallery')
+
+const insertedGallery = galleryImages.forEach(function(image){
+    return imageGallery.innerHTML += `<img src=${image}>`
+})
+
+
+
