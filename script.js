605e0a8327d8c36d58156a01f904f54b62e00024
joshuaforster <110224330+joshuaforster@users.noreply.github.com>
2023-11-11 22:02:31 +0000

    push

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
-// Navigation Menu
+// ======================================
+// Hamburger Menu Section
+// ======================================
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener("click", function {
+hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 });
 
-document.querySelectorAll(".nav-link").forEach(function {
-  link.addEventListener("click", () function {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-  });
-});
-
-
-
-
-
-
-
-
+document.querySelectorAll(".nav-link").forEach(m => m.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}));
+// ======================================
+// Hamburger Menu End
+// ======================================
