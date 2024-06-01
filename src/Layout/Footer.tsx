75c611f5d23271c42745f872ef87665401ac5ddd
joshuaforster <110224330+joshuaforster@users.noreply.github.com>
2024-06-01 15:31:52 +0100

    first commit

--- src/Layout/Footer.tsx
+++ src/Layout/Footer.tsx
@@ -3,19 +3,12 @@ import { Link } from 'react-router-dom';
 import { useTheme } from '../CustomComponents/darkmode';
 
 export default function Footer() {
-  type NavLinks = {
-    name: string;
-    path?: string;
-    type?: string;
-  };
-
-  const navigationLinks: NavLinks[] = [
-    { name: 'Home', path: '/' },
-    { name: 'About', path: '/about' },
-    { name: 'Gallery', path: '/gallery' },
-    { name: 'Contact', path: '/contact' }
+  const navigationLinks = [
+    { name: 'Home', path: '#hero' },
+    { name: 'About', path: '#about' },
+    { name: 'Portfolio', path: '#portfolio' },
+    { name: 'Contact', path: '#contact' }
   ];
-
 
   const legalLinks = [
     { name: "Privacy Policy", path: "/privacy-policy" },
@@ -26,16 +19,23 @@ export default function Footer() {
 
   const currentYear = new Date().getFullYear();
 
+  const handleLinkClick = (path:any) => {
+    if (window.location.pathname !== '/') {
+      window.location.href = '/' + path;
+    }
+  };
+
+
   return (
-    <footer className="p-4 bg-gray-100 sm:p-6 dark: dark:bg-dark-gray">
+    <footer className="p-4 bg-gray-100 sm:p-6 dark:bg-dark-gray">
        <div className="border-t border-gray-200 dark:border-white sm:pt-8"></div>
       <div className="mx-auto max-w-screen-xl">
         <div className="md:flex md:justify-between">
           <div className="flex flex-col items-center mb-6 md:mb-0 md:items-start md:w-1/2">
             <Link to="/" className="flex items-center mb-4 md:mb-0">
               <img
-                src={theme === 'dark' ? '/images/HollyHipwelldark.png':'/images/HollyHipwell.png'}
-                className="h-8 sm:h-6"
+                 src={theme === 'dark' ? '/images/LukeLogoDark.png' : '/images/LukeLogo.webp'}
+                className="h-184 sm:h-14"
                 alt="Company Logo"
               />
             </Link>
@@ -43,24 +43,20 @@ export default function Footer() {
           <div className="flex flex-wrap justify-between md:w-1/2">
             {/* Navigation Section */}
             <div className="w-full sm:w-1/2 mb-6 md:mb-0 text-center md:text-left">
-            <div className=" md:hidden border-t border-gray-200 dark:border-gray-400 pt-8 my-4"></div>
+              <div className="md:hidden border-t border-gray-200 dark:border-gray-400 pt-8 my-4"></div>
               <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Navigation</h2>
               <ul className="text-gray-600 dark:text-white">
                 {navigationLinks.map((link, index) => (
-                  link.path ? (
-                    <li key={index} className="mb-4">
-                      <Link to={link.path} className="hover:underline">{link.name}</Link>
-                    </li>
-                  ) : null
+                  <li key={index} className="mb-4">
+                    <a href={link.path} className="hover:underline">{link.name}</a>
+                  </li>
                 ))}
               </ul>
             </div>
 
-      
-
             {/* Legal Section */}
             <div className="w-full sm:w-1/2 text-center md:text-left">
-            <div className=" md:hidden border-t border-gray-200 dark:border-gray-400 pt-8 my-4"></div>
+              <div className="md:hidden border-t border-gray-200 dark:border-gray-400 pt-8 my-4"></div>
               <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
               <ul className="text-gray-600 dark:text-white">
                 {legalLinks.map((link, index) => (
@@ -74,11 +70,9 @@ export default function Footer() {
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <div className="sm:flex sm:items-center sm:justify-between text-center">
-          <span className="text-sm text-gray-500 sm:text-center dark:text-white">© {currentYear} <Link to="/" className="hover:underline">Holly Hipwell™</Link>. All Rights Reserved.</span>
+          <span className="text-sm text-gray-500 sm:text-center dark:text-white">© {currentYear} <Link to="/" className="hover:underline">Luke Sorrell™</Link>. All Rights Reserved.</span>
         </div>
       </div>
-    
-    
     </footer>
   );
 }
--- src/MainPages/Home.tsx
+++ src/MainPages/Home.tsx
@@ -1,82 +1,135 @@
 import React from "react";
 import Hero from "../PageComponents/Hero";
 import TestimonialSlider from "../PageComponents/Reviews";
-import Button from "../CustomComponents/buttons";
 import { useTheme } from '../CustomComponents/darkmode';
+import LogoSection from "../CustomComponents/logoSection";
+import Skills from "../CustomComponents/Skills";
+import WhyMe from "../CustomComponents/whyme";
+import About from "../PageComponents/about";
+import Portfolio from "../PageComponents/portfolio";
+import Contact from "../PageComponents/Contact";
 
 export default function Home() {
     const { theme } = useTheme();
 
+    const logos = theme === 'dark' ? [
+        { src: '/images/NUALogo.webp', alt: 'Laura Blake' },
+        { src: '/images/InvokeLogo.webp', alt: 'Franks' },
+        { src: '/images/A&DLogo.webp', alt: 'Anteros' },
+    ] : [
+        { src: '/images/nuadark.png', alt: 'Laura Blake' },
+        { src: '/images/Invoke Digital Signage Logo dark.png', alt: 'Franks' },
+        { src: '/images/A&D_Film_Studios_dark.png', alt: 'Anteros' },
+    ];
+
+
+
+    const skills = [
+        {
+            imgSrc: "images/letter.webp",
+            imgAlt: "Abstract art piece 1",
+            title: "Typography",
+            description: "Crafting visuals that captivate, I bridge design and message for impactful brand presence",
+        },
+        {
+            imgSrc: "images/pencil.webp",
+            imgAlt: "Rabbit art piece",
+            title: "Typography",
+            description: "Crafting visuals that captivate, I bridge design and message for impactful brand presence",
+        },
+        {
+            imgSrc: "images/stylus.webp",
+            imgAlt: "Abstract art piece 2",
+            title: "Typography",
+            description: "Crafting visuals that captivate, I bridge design and message for impactful brand presence",
+        },
+    ];
+
+    const about = {
+        headline: 'About Me',
+        image: '/images/Luke_Sorrell_Avater_2023.webp',
+        body: `
+            I developed my skills after graduating with my undergrad degree in animation at the Norwich University of the Arts in 2016. As a freelancer and motion graphic designer, I gained experience in the graphic design industry. I noticed I had a passion for graphic design, which led me to complete a master's in communication design. My practice is motion graphics due to my degree in animation; I always want to see what my designs look like when they apply movement. Thanks to that experience, I can make my work look energetic and approachable.
+
+            I always try to keep an open mind and take people's opinions and suggestions on board when developing my designs. I believe this makes me a better designer while pushing myself to achieve that next level of work. It also allows me to interact with those people viewing my creative work.
+
+            My strength has always been to come up with new ideas on the spot and figure out how to implement them into my designs. This leads me to play with different ideas, which I enjoy even in my spare time. I also try to inject humour and personality into my work to help people connect to my designs. This year in my master's, I've discovered why I love graphic design, which has given me a new drive to succeed and reach my potential.
+        `
+    };
+
+    const skillsSection = {
+        headline: "Our Skills",
+        desc: "We offer a diverse range of skills to meet your needs.",
+    };
+    const whyMe = {
+        headline: "Why Me",
+        text: "Driven by a passion for visual storytelling, I merge design, animation, and typography to elevate brands. With every project, I commit to delivering tailored solutions that resonate and engage. Choose collaboration, creativity, and a distinct voice in a saturated digital landscape"
+    };
+    
+    const portfolioPieces = [
+        {
+            heading: "Best website collections",
+            text: "Website",
+            image: "/images/Dyslexia.webp",
+            link: "https://www.dropbox.com/scl/fo/fp04e5eil1m85bv9bwflw/h/Norwich%20University%20of%20the%20Arts/MA%20COMMUNICATION%20DESIGN?dl=0&preview=Dyslexia_Is_Not_A_Disability_2022_v2.mp4&subfolder_nav_tracking=1"
+        },
+        {
+            heading: "Block of UI kit collections",
+            text: "UI Kit",
+            image: "/images/haunted.webp",
+            link: "https://www.youtube.com/watch?v=0KXVhueCx4k"
+        },
+        {
+            heading: "Ton’s of mobile mockup",
+            text: "Mockups",
+            image: '/images/Reviews.webp',
+            link: "https://www.dropbox.com/scl/fo/fp04e5eil1m85bv9bwflw/h/Norwich%20University%20of%20the%20Arts/MA%20COMMUNICATION%20DESIGN/MP_Posters_2023?dl=0&subfolder_nav_tracking=1"
+        },
+        {
+            heading: "Huge collection of animation",
+            text: "Animation",
+            image: "/images/Typography.webp",
+            link: "https://www.dropbox.com/scl/fo/fp04e5eil1m85bv9bwflw/h/Norwich%20University%20of%20the%20Arts/MA%20COMMUNICATION%20DESIGN/Typography_Project_2023?dl=0&subfolder_nav_tracking=1"
+        },
+        {
+            heading: "Huge collection of animation",
+            text: "Animation",
+            image: "/images/loreal.webp",
+            link: "https://www.youtube.com/watch?v=qHTvaAztvWI"
+        },
+        {
+            heading: "Huge collection of animation",
+            text: "Animation",
+            image: "/images/cafe.webp",
+            link: "https://www.dropbox.com/scl/fo/fp04e5eil1m85bv9bwflw/h/Freelance%20Work?dl=0&subfolder_nav_tracking=1"
+        }
+    ];
+    
     return (
         <section>
-            <Hero />
-            <section className="bg-white dark:bg-dark-gray">
-                <div className="mx-auto max-w-screen-xl px-4 py-8 lg:gap-8 xl:gap-0">
-                    <div className="text-center mx-auto max-w-screen-xl px-4 justify-center items-center mb-8">
-                        <h2 className="text-4xl font-bold text-gray-900 dark:text-white">Featured With...</h2>
-                    </div>
-                    <div className="flex flex-col md:flex-row justify-between items-center space-y-8 md:space-y-0 md:space-x-8">
-                        <img src={theme === 'dark' ? "/images/laurablakedark.png" : "/images/laurablake.png"} alt="Laura Blake" className="w-1/2 md:w-1/6" />
-                        <img src={theme === "dark" ? "/images/franksbardark.png" : "/images/franks.png"} alt="Franks" className="w-1/2 md:w-1/6" />
-                        <img src={theme === "dark" ? "/images/anterosdark.png" : "/images/anteros.png"} alt="Anteros" className="w-1/2 md:w-1/6" />
-                    </div>
-                </div>
-            </section>
-
-            <div className="bg-white dark:bg-dark-gray">
-                <div className="mx-auto max-w-screen-xl px-4 py-8 lg:gap-8 xl:gap-0 md:grid-cols-12">
-                    <div className="flex flex-wrap -mx-4">
-                        <div className="w-full md:w-1/3 px-4 mb-8 md:mb-0">
-                            <div className="h-[500px] overflow-hidden relative">
-                                <img
-                                    src="images/Group 44.png"
-                                    alt="Abstract art piece 1"
-                                    className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 object-cover w-full h-full"
-                                />
-                            </div>
-                        </div>
-                        <div className="w-full md:w-1/3 px-4 mb-8 md:mb-0">
-                            <div className="h-[500px] overflow-hidden relative">
-                                <img
-                                    src="images/rabbit.webp"
-                                    alt="Rabbit art piece"
-                                    className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 object-cover w-full h-full"
-                                />
-                            </div>
-                        </div>
-                        <div className="w-full md:w-1/3 px-4 mb-8 md:mb-0">
-                            <div className="h-[500px] overflow-hidden relative">
-                                <img
-                                    src="images/abstractmockup.webp"
-                                    alt="Abstract art piece 2"
-                                    className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 object-cover w-full h-full"
-                                />
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div id="hero">
+                <Hero />
             </div>
-
-            <section className="bg-white dark:bg-dark-gray">
-                <div className="mx-auto max-w-screen-xl px-4 lg:gap-8 xl:gap-0 flex justify-center items-center h-full py-8">
-                    <Button variant="primary" to="/contact">
-                        Get In Contact
-                    </Button>
-                </div>
-            </section>
-
-            <section className="bg-white dark:bg-dark-gray py-16">
-                <div className="text-center mx-auto max-w-screen-xl px-4 justify-center items-center">
-                    <p className="lg:text-5xl text-4xl italic text-gray-900 dark:text-white leading-loose lg:leading-loose">
-                        “Enjoy the little things, for one day you may look back and realize they were the big things.”
-                    </p>
-                    <p className="text-lg text-gray-600 dark:text-gray-400 mt-2">
-                        – Robert Brault
-                    </p>
-                </div>
-            </section>
-
-            <TestimonialSlider />
+            <div id="logos">
+                <LogoSection logos={logos} theme={theme} />
+            </div>
+            <div id="skills">
+                <Skills skills={skills} headline={skillsSection.headline} desc={skillsSection.desc} />
+            </div>
+            <div id="whyme">
+                <WhyMe headline={whyMe.headline} text={whyMe.text}/>
+            </div>
+            <div id="portfolio">
+                <Portfolio portfolioPieces={portfolioPieces} Header="Our Portfolio" />
+            </div>
+            <div id="about">
+                <About headline={about.headline} body={about.body} image={about.image}/>
+            </div>
+            <div id="reviews">
+                <TestimonialSlider />
+            </div>
+            <div id="contact">
+                <Contact />
+            </div>
         </section>
-    );
-}
+    );}
